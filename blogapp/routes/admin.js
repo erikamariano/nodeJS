fd0e55a85e97a5407d7e60d3608e2e3b289029a7
erika <erikammariano@gmail.com>
2020-09-29 23:06:30 -0300

    first commit

---
+++ blogapp/routes/admin.js
@@ -0,0 +1,22 @@
+//Documento para guardar todas a sminhas rotas da página admin
+const express = require("express");
+const router = express.Router();
+
+//em vez de usar app.get, usa-se router.get
+router.get('/', (req,res) => {
+    res.render("admin/index")
+})
+
+router.get('/post', (req,res) => {
+    res.send("Página de Posts!")
+})
+
+router.get('/categorias', (req,res) => {
+    res.render('admin/categorias')
+})
+
+router.get('/categorias/add', (req, res) => {
+    res.render('admin/addcategorias')
+})
+
+module.exports = router;
